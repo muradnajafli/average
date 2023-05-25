76b8c80be58a5bbd91f7b1460a66bd60b1271da6
Murad Najafli <muradnecefli05@gmail.com>
2023-05-25 11:55:53 +0400

    AvarageTaskCommit2

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -5,31 +5,29 @@ import java.util.Scanner;
 public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt();
 
-        if (size < 100) {
-            int[] arrays = new int[size];
-            int total = 0;
-            int avarage = 0;
-            for (int i = 0; i < size; i++) {
-                arrays[i] = scanner.nextInt();
+        int count = 0;
+        int total = 0;
+        int num;
+        while (true) {
+            num = scanner.nextInt();
+            if (num == 0) {
+                break;
             }
 
-            if (arrays[size-1] == 0) {
-                for (int array: arrays) {
-                    total+=array;
-                }
-                avarage = total / (size-1);
-                System.out.println(avarage);
-
-
-            } else {
-                System.out.println("Last element must be 0");
-            }
-        } else {
-            System.out.println("Size of array can have a maximum 100");
-
+            count++;
+            total += num;
         }
 
+        scanner.close();
+
+        int average;
+        if (count == 0) {
+            average = 0;
+        } else {
+            average = total / count;
+        }
+
+        System.out.println(average);
     }
 }
